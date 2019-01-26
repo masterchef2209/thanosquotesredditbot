d559a095e43f83c6aa1fae388e3d652f366ba30e
Hidayat Khan <huk2209@gmail.com>
2019-01-26 18:55:15 +0530

    bug fix

--- bot.py
+++ bot.py
@@ -64,9 +64,8 @@ def run_bot(r, comments_replied_to):
             if re.search("thanos quote", comment.body, re.IGNORECASE):
                 print(comment.body)
                 global count
-                count=(count+1)%(int(len(thanos_quotes)))
-                thanos_reply = thanos_quotes[count]
-                comment.reply(thanos_quotes)
+                thanos_reply = random.choice(thanos_quotes)
+                comment.reply(thanos_reply)
                 if comment.id not in comments_replied_to:
                     comments_replied_to.append(comment.id)
                     with open("comments_replied_to.txt", "a") as f:
@@ -86,7 +85,7 @@ def get_saved_comments():
 
     return comments_replied_to
 
-count=0
+
 r = bot_login()
 comments_replied_to = get_saved_comments()
 while True:
